d28c58030d77fd33f6116c372ec803ce522c38ad
AntonyHabib <aeh25@njit.edu>
2022-11-14 21:59:20 -0500

    rebuilt sql all over again

--- public_html/project/sql/009_order_items.sql
+++ public_html/project/sql/009_order_items.sql
@@ -1,12 +1,11 @@
 CREATE TABLE IF NOT EXISTS `OrderItems`(
     `id` int AUTO_INCREMENT PRIMARY KEY,
     `order_id` int,
-    `unit_price` INT DEFAULT 999,
-    `quantity` int,
     `product_id` int,
+    `quantity` int, 
+    `unit_price` INT DEFAULT 999,
     `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     FOREIGN KEY (product_id) REFERENCES Products(id),
-    FOREIGN KEY (order_id) REFERENCES Orders(id),
-    check (quantity >= 0)
+    FOREIGN KEY (order_id) REFERENCES Orders(id)
 )
