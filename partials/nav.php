2f3416920df7468acf5f82a7fd43fe7615e8fc6c
AntonyHabib <aeh25@njit.edu>
2022-12-14 08:48:21 -0500

    everything is done

--- partials/nav.php
+++ partials/nav.php
@@ -21,9 +21,8 @@ if (($localWorks && $domain == "localhost") || $domain != "localhost") {
     ]);
 }
 session_start();
-
-
 ?>
+
 <!-- include css and js files -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -41,6 +40,7 @@ session_start();
 
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('profiles.php'); ?>">Profile</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('shop.php'); ?>">Shop</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('cart.php'); ?>">Cart</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('Checkout.php'); ?>">Checkout</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('history.php'); ?>">View History</a></li>
                     
@@ -49,12 +49,16 @@ session_start();
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('login.php'); ?>">Login</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('register.php'); ?>">Register</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('shop.php'); ?>">Shop</a></li>
+
                 <?php endif; ?>
                 <?php if (has_role("Admin")) : ?>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('admin/list_purchase_history.php'); ?>"> The Users History</a> </li>
+
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="rolesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Admin Roles
                         </a>
+
                         
                         <ul class="dropdown-menu bg-warning" aria-labelledby="rolesDropdown">
                             <li><a class="dropdown-item" href="<?php echo get_url('admin/create_role.php'); ?>">Create</a></li>
@@ -71,12 +75,11 @@ session_start();
                             <li><a class="dropdown-item" href="<?php echo get_url('admin/list_items.php'); ?>">List</a></li>
 
                         </ul>
-                        
                     </li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('admin/list_purchase_history.php'); ?>">View History</a> </li>
                 <?php endif; ?>
                 <?php if (is_logged_in()) : ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
+
                 <?php endif; ?>
             </ul>
         </div>
